394a34d55affb0f86c763eb9a588a3566cc81ab7
Aubreykun <150863259+Aubreykun@users.noreply.github.com>
2023-11-15 18:37:10 -0500

    Update TrashLoot_Fashion.lua
    fixed naming in the event register part

--- TrashLoot_Fashion.lua
+++ TrashLoot_Fashion.lua
@@ -1,9 +1,9 @@
 DEFAULT_CHAT_FRAME:AddMessage("TrashLoot Fashion Coin Edition is Active", 1, 0, 1);
-TrashLoot_STSC = CreateFrame("Frame")
-TrashLoot_STSC:RegisterEvent("START_LOOT_ROLL") 
-TrashLoot_STSC:RegisterEvent("LOOT_BIND_CONFIRM")
-TrashLoot_STSC:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-TrashLoot_STSC:SetScript("OnEvent", function()
+TrashLoot_Fashion = CreateFrame("Frame")
+TrashLoot_Fashion:RegisterEvent("START_LOOT_ROLL") 
+TrashLoot_Fashion:RegisterEvent("LOOT_BIND_CONFIRM")
+TrashLoot_Fashion:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+TrashLoot_Fashion:SetScript("OnEvent", function()
     if event == "START_LOOT_ROLL" then
         local rollID, rollTime, rollItemLink, rollQuantity, rollPlayer = arg1, arg2, arg3, arg4, arg5
 		local texture, name, count, quality = GetLootRollItemInfo(rollID)
